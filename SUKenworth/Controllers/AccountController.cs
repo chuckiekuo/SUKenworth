1f02ae905277fa347b876d88a93b472cadf37622
Troy Osland <oslandt@seattleu.edu>
2017-11-09 14:52:32 -0800

    Fixed "" to String.Empty to meet readability standards

--- SUKenworth/Controllers/AccountController.cs
+++ SUKenworth/Controllers/AccountController.cs
@@ -88,7 +88,7 @@ namespace SUKenworth.Controllers
                     return RedirectToAction("SendCode", new { ReturnUrl = returnUrl, RememberMe = model.RememberMe });
                 case SignInStatus.Failure:
                 default:
-                    ModelState.AddModelError("", "Invalid login attempt.");
+                    ModelState.AddModelError(String.Empty, "Invalid login attempt.");
                     return View(model);
             }
         }
