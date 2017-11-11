ecf57c50c671c57f6681c22910f55a85acf46df0
William Gross <grossw@seattleu.edu>
2017-11-10 21:17:08 -0800

    Added frame for Dataconfig class
    Methods are currently empty.

---
+++ SUKenworth/App_Start/DataConfig.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Database_Sandbox.Models;
+
+namespace Database_Sandbox.App_Start
+{
+    public static class DataConfig
+    {
+        public static TestDataBaseEntities db;
+
+        static DataConfig()
+        {
+
+        }
+
+        public static void DirectCreate(User user)
+        {
+
+        }
+
+        public static void BulkDirectCreate(IEnumerable<User> inputList)
+        {
+
+        }
+
+        public static void DirectDelete(User user)
+        {
+
+        }
+
+        public static void DirectUpdate(User user)
+        {
+
+        }
+
+        public static void ClearDataBase()
+        {
+
+        }
+
+        public static List<User> GetDataList()
+        {
+
+        }
+    }
+}
