961bbb2b406d8ebb1c04a432f9f9babdd08d98b3
Jason Esparza <esparza2@seattleu.edu>
2017-11-13 10:55:48 -0800

    Test comment for git

--- SUKenworth/Global.asax.cs
+++ SUKenworth/Global.asax.cs
@@ -8,6 +8,7 @@ using System.Web.Routing;
 
 namespace SUKenworth
 {
+    //This is a test
     public class MvcApplication : System.Web.HttpApplication
     {
         protected void Application_Start()
