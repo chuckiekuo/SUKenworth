2161cb42db5942c412826c1845157d07e264ebff
William Gross <grossw@seattleu.edu>
2017-11-14 13:31:13 -0800

    removed System.Xml.Linq
    This package was causing a run-time error and so it has been removed.

--- SUKenworth/Models/TestDataModels/User.cs
+++ SUKenworth/Models/TestDataModels/User.cs
@@ -15,6 +15,8 @@ namespace SUKenworth.Models.TestDataModels
     public partial class User
     {
         public int Id { get; set; }
-        public string Name { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public bool Admin { get; set; }
     }
 }
