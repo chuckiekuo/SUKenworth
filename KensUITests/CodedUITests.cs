27b2c454447f3b81f0e076cb2cd9c404c29d58ca
Troy Osland <oslandt@seattleu.edu>
2017-12-08 21:06:57 -0800

    Moved a login test from codedUITests to LoginTests

--- KensUITests/CodedUITests.cs
+++ KensUITests/CodedUITests.cs
@@ -31,131 +31,6 @@ namespace KensUITests
             // To generate code for this test, select "Generate Code for Coded UI Test" from the shortcut menu and select one of the menu items.
         }
 
-   
-
-        [TestMethod]
-        public void ValidLogInNotAdmin()
-        {
-            //!Make sure to add the path to where you extracting the chromedriver.exe:
-            using (IWebDriver driver = new ChromeDriver(Extensions.ChromeDriverLocation))//<-Add your path
-            {
-                //set an implicit wait time before any search for an item fails
-                driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(Extensions.MaxWaitTime);
-
-                //navigate to the kenworth page
-                driver.Navigate().GoToUrl(Extensions.BaseUrl);
-
-                //check that page is the right page
-                ValidatePageTransition(driver, "Account", "Login");
-
-
-                //find the email box to input username info
-                IWebElement emailBox = driver.FindElement(By.Id("Email"));
-
-                //enter a valid email to login
-                emailBox.SendKeys(Extensions.ValidEmailNotAdmin1);
-
-                //find the password box to input password info    
-                IWebElement passwordBox = driver.FindElement(By.Id("Password"));
-
-                //enter a valid password
-                passwordBox.SendKeys(Extensions.ValidPasswordNotAdmin1);
-
-                //submit
-                driver.FindElement(By.Name("LogInSubmit")).Click();
-
-                //check that page is the right page
-                //i dunno know what id's to look for, i cannot navigate to the page that naturally comes after a log in
-
-            }
-
-        }
-
-        [TestMethod]
-        public void NoLogIn()
-        {
-            //!Make sure to add the path to where you extracting the chromedriver.exe:
-            using (IWebDriver driver = new ChromeDriver(Extensions.ChromeDriverLocation))
-            {
-                //set an implicit wait time before any search for an item fails
-                driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(Extensions.MaxWaitTime);
-
-                //navigate to the kenworth page
-                driver.Navigate().GoToUrl(Extensions.BaseUrl);
-
-                //check that page is the right page
-                driver.FindElement(By.Id("Page-Done"));
-                driver.FindElement(By.Id("Area--Done"));
-                driver.FindElement(By.Id("Controller-Account-Done"));
-                driver.FindElement(By.Id("View-Login-Done"));
-
-
-                //click register, should take you to register page
-                driver.FindElement(By.Id("registerLink")).Click();
-
-                //check that page is the right page
-                driver.FindElement(By.Id("Page-Done"));
-                driver.FindElement(By.Id("Area--Done"));
-                driver.FindElement(By.Id("Controller-Account-Done"));
-                driver.FindElement(By.Id("View-Register-Done"));
-
-                //click log in, should take you to the log in page again
-                driver.FindElement(By.Id("loginLink")).Click();
-
-                //check that page is the right page
-                driver.FindElement(By.Id("Page-Done"));
-                driver.FindElement(By.Id("Area--Done"));
-                driver.FindElement(By.Id("Controller-Account-Done"));
-                driver.FindElement(By.Id("View-Login-Done"));
-
-                //click register as a new user link on login page
-                driver.FindElement(By.Id("registerLinkOnPage")).Click();
-
-                //check that page is the right page
-                driver.FindElement(By.Id("Page-Done"));
-                driver.FindElement(By.Id("Area--Done"));
-                driver.FindElement(By.Id("Controller-Account-Done"));
-                driver.FindElement(By.Id("View-Register-Done"));
-
-                //click contact, should take you to contact page
-                driver.FindElement(By.Id("contactLinkNavBar")).Click();
-
-                //check that page is the right page
-                driver.FindElement(By.Id("Page-Done"));
-                driver.FindElement(By.Id("Area--Done"));
-                driver.FindElement(By.Id("Controller-Home-Done"));
-                driver.FindElement(By.Id("View-Contact-Done"));
-
-                //click about, should take you to about page
-                driver.FindElement(By.Id("aboutLinkNavBar")).Click();
-
-                //check that page is the right page
-                driver.FindElement(By.Id("Page-Done"));
-                driver.FindElement(By.Id("Area--Done"));
-                driver.FindElement(By.Id("Controller-Home-Done"));
-                driver.FindElement(By.Id("View-About-Done"));
-
-                //click SetDatabase, should take you to the set database page
-                //click contact, should take you to contact page
-                driver.FindElement(By.Id("setDatabaseNavBar")).Click();
-
-                //check that page is the right page
-                driver.FindElement(By.Id("Page-Done"));
-                driver.FindElement(By.Id("Area--Done"));
-                driver.FindElement(By.Id("Controller-Home-Done"));
-                driver.FindElement(By.Id("View-SetDatabase-Done"));
-
-                //click homescreen logo in top left, should take you to homepage(since not logged in this should be log in page)
-                //click contact, should take you to contact page
-                driver.FindElement(By.Id("homeLinkNavBar")).Click();
-
-                //check that page is the right page
-                //i don't know what id's to look for, link is broken
-
-
-            }
-        }
-
         [TestMethod]
         public void ValidLogInAndLogOffAdmin()
         {
@@ -254,7 +129,7 @@ namespace KensUITests
             }
         }
         //add happy path tests for
-        // clicking any and all links
+        // clicking any and all links - logged in or not
         //   toggling admin privleges
         // register a user
         //  needs a test db that can be wiped for that
