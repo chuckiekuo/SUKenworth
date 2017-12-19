6b88675cca33710f8eab4166aac146f0d470cf06
Troy Osland <oslandt@seattleu.edu>
2017-12-18 18:16:08 -0800

    refactored a test to properly use assembly file

--- KensUITests/Views/Accounts/LoginTest.cs
+++ KensUITests/Views/Accounts/LoginTest.cs
@@ -48,36 +48,31 @@ namespace KensUITests.Views.Accounts
         [TestMethod]
         public void Login_Valid_NonAdminLogin_Should_JumpToHomepage()
         {
-            //!Make sure to add the path to where you extracting the chromedriver.exe:
-            using (IWebDriver driver = new ChromeDriver(Extensions.ChromeDriverLocation))//<-Add your path
-            {
-                //set an implicit wait time before any search for an item fails
-                driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(Extensions.MaxWaitTime);
+            NavigateToPage(AssemblyTests.CurrentDriver, _Controller, _Action);
 
-                NavigateToPage(driver, _Controller, _Action);
+            //find the email box to input username info
+            IWebElement emailBox = AssemblyTests.CurrentDriver.FindElement(By.Id(Extensions.LoginEmailInputBoxIdTag));
 
-                //find the email box to input username info
-                IWebElement emailBox = driver.FindElement(By.Id(Extensions.LoginEmailInputBoxIdTag));
+            //enter a valid email to login
+            emailBox.SendKeys(Extensions.ValidEmailNotAdmin1);
 
-                //enter a valid email to login
-                emailBox.SendKeys(Extensions.ValidEmailNotAdmin1);
+            //find the password box to input password info    
+            IWebElement passwordBox = AssemblyTests.CurrentDriver.FindElement(By.Id(Extensions.LoginPasswordInputBoxIdTag));
 
-                //find the password box to input password info    
-                IWebElement passwordBox = driver.FindElement(By.Id(Extensions.LoginPasswordInputBoxIdTag));
+            //enter a valid password
+            passwordBox.SendKeys(Extensions.ValidPasswordNotAdmin1);
 
-                //enter a valid password
-                passwordBox.SendKeys(Extensions.ValidPasswordNotAdmin1);
+            //submit
+            AssemblyTests.CurrentDriver.FindElement(By.Id(Extensions.LoginSubmitButtonIdTag)).Click();
 
-                //submit
-                driver.FindElement(By.Id(Extensions.LoginSubmitButtonIdTag)).Click();
-
-                //check that page is the right page
-                ValidatePageTransition(driver, "Home", "Index");
-            }
+            //check that page is the right page
+            ValidatePageTransition(AssemblyTests.CurrentDriver, "Home", "Index");
+            
 
         }
 
         
-        //
+        //add tests for remeber me functionality
+
     }
 }
