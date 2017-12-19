3c8b13fa62656fd537cd01c02c3819207dd132c8
Troy Osland <oslandt@seattleu.edu>
2017-12-18 18:08:28 -0800

    Refactored a test to properly use assembly file

--- KensUITests/Views/Accounts/LoginTest.cs
+++ KensUITests/Views/Accounts/LoginTest.cs
@@ -23,35 +23,25 @@ namespace KensUITests.Views.Accounts
         [TestMethod]
         public void Login_Valid_AdminLogin_Should_JumpToHomepage()
         {
-
-            //!Make sure to add the path to where you extracting the chromedriver.exe:
-            using (IWebDriver driver = new ChromeDriver(Extensions.ChromeDriverLocation))//<-Add your path
-            {
-                //set an implicit wait time before any search for an item fails
-                driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(Extensions.MaxWaitTime);
-
-                NavigateToPage(driver, _Controller, _Action);
+                 NavigateToPage(AssemblyTests.CurrentDriver, _Controller, _Action);
 
                 //find the email box to input username info
-                IWebElement emailBox = driver.FindElement(By.Id(Extensions.LoginEmailInputBoxIdTag));
+                IWebElement emailBox = AssemblyTests.CurrentDriver.FindElement(By.Id(Extensions.LoginEmailInputBoxIdTag));
 
                 //enter a valid email to login
                 emailBox.SendKeys(Extensions.ValidEmailAdmin1);
 
                 //find the password box to input password info    
-                IWebElement passwordBox = driver.FindElement(By.Id(Extensions.LoginPasswordInputBoxIdTag));
+                IWebElement passwordBox = AssemblyTests.CurrentDriver.FindElement(By.Id(Extensions.LoginPasswordInputBoxIdTag));
 
                 //enter a valid password
                 passwordBox.SendKeys(Extensions.ValidPasswordAdmin1);
 
-                //submit
-                driver.FindElement(By.Id(Extensions.LoginSubmitButtonIdTag)).Click();
+            //submit
+            AssemblyTests.CurrentDriver.FindElement(By.Id(Extensions.LoginSubmitButtonIdTag)).Click();
 
                 //check that page is the right page
-                ValidatePageTransition(driver, "Home", "Index");
-
-            }
-
+                ValidatePageTransition(AssemblyTests.CurrentDriver, "Home", "Index");
         }
 
 
