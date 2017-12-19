2bee33a996a2c07d493622b57a2cea58efed855d
Troy Osland <oslandt@seattleu.edu>
2017-12-18 19:34:03 -0800

    refactored the test that clicked all links on login page

--- KensUITests/Views/Accounts/LoginTest.cs
+++ KensUITests/Views/Accounts/LoginTest.cs
@@ -126,90 +126,69 @@ namespace KensUITests.Views.Accounts
         [TestMethod]
         public void NoLogin_Valid_Should_VisitAllAllowedPages()
         {
-            //!Make sure to add the path to where you extracting the chromedriver.exe:
-            using (IWebDriver driver = new ChromeDriver(Extensions.ChromeDriverLocation))
-            {
-                //set an implicit wait time before any search for an item fails
-                driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(Extensions.MaxWaitTime);
+            //navigate to the kenworth page
+            NavigateToPage(LoginAssemblyTests.CurrentDriver, _Controller, _Action);
 
-                //navigate to the kenworth page
-                driver.Navigate().GoToUrl(Extensions.BaseUrl);
+            //click register, should take you to register page
+            LoginAssemblyTests.CurrentDriver.FindElement(By.Id("registerLink")).Click();
 
-                //check that page is the right page
-                driver.FindElement(By.Id("Page-Done"));
-                driver.FindElement(By.Id("Area--Done"));
-                driver.FindElement(By.Id("Controller-Account-Done"));
-                driver.FindElement(By.Id("View-Login-Done"));
+            //check that page is the right page (should be register page)
+            ValidatePageTransition(LoginAssemblyTests.CurrentDriver, "Account", "Register");
 
 
-                //click register, should take you to register page
-                driver.FindElement(By.Id("registerLink")).Click();
+            //click log in, should take you to the log in page again
+            LoginAssemblyTests.CurrentDriver.FindElement(By.Id("loginLink")).Click();
 
-                //check that page is the right page
-                driver.FindElement(By.Id("Page-Done"));
-                driver.FindElement(By.Id("Area--Done"));
-                driver.FindElement(By.Id("Controller-Account-Done"));
-                driver.FindElement(By.Id("View-Register-Done"));
+            //click register as a new user link on login page
+            LoginAssemblyTests.CurrentDriver.FindElement(By.Id("registerLinkOnPage")).Click();
 
-                //click log in, should take you to the log in page again
-                driver.FindElement(By.Id("loginLink")).Click();
-
-                //check that page is the right page
-                driver.FindElement(By.Id("Page-Done"));
-                driver.FindElement(By.Id("Area--Done"));
-                driver.FindElement(By.Id("Controller-Account-Done"));
-                driver.FindElement(By.Id("View-Login-Done"));
-
-                //click register as a new user link on login page
-                driver.FindElement(By.Id("registerLinkOnPage")).Click();
-
-                //check that page is the right page
-                driver.FindElement(By.Id("Page-Done"));
-                driver.FindElement(By.Id("Area--Done"));
-                driver.FindElement(By.Id("Controller-Account-Done"));
-                driver.FindElement(By.Id("View-Register-Done"));
-
-                //click contact, should take you to contact page
-                driver.FindElement(By.Id("contactLinkNavBar")).Click();
-
-                //check that page is the right page
-                driver.FindElement(By.Id("Page-Done"));
-                driver.FindElement(By.Id("Area--Done"));
-                driver.FindElement(By.Id("Controller-Home-Done"));
-                driver.FindElement(By.Id("View-Contact-Done"));
-
-                //click about, should take you to about page
-                driver.FindElement(By.Id("aboutLinkNavBar")).Click();
-
-                //check that page is the right page
-                driver.FindElement(By.Id("Page-Done"));
-                driver.FindElement(By.Id("Area--Done"));
-                driver.FindElement(By.Id("Controller-Home-Done"));
-                driver.FindElement(By.Id("View-About-Done"));
-
-                //click SetDatabase, should take you to the set database page
-                //click contact, should take you to contact page
-                driver.FindElement(By.Id("setDatabaseNavBar")).Click();
-
-                //check that page is the right page
-                driver.FindElement(By.Id("Page-Done"));
-                driver.FindElement(By.Id("Area--Done"));
-                driver.FindElement(By.Id("Controller-Home-Done"));
-                driver.FindElement(By.Id("View-SetDatabase-Done"));
-
-                //click homescreen logo in top left, should take you to homepage(since not logged in this should be log in page)
-                //click contact, should take you to contact page
-                driver.FindElement(By.Id("homeLinkNavBar")).Click();
-
-                //check that page is the right page
-                //i don't know what id's to look for, link is broken
+            //check that page is the right page (should be register page)
+            ValidatePageTransition(LoginAssemblyTests.CurrentDriver, "Account", "Register");
 
 
-            }
+            //click log in, should take you to the log in page again
+            LoginAssemblyTests.CurrentDriver.FindElement(By.Id("loginLink")).Click();
+
+            //click contact, should take you to contact page
+            LoginAssemblyTests.CurrentDriver.FindElement(By.Id("contactLinkNavBar")).Click();
+
+            //check that page is the right page (Should be contact page)
+            ValidatePageTransition(LoginAssemblyTests.CurrentDriver, "Home", "Contact");
+
+
+            //click log in, should take you to the log in page again
+            LoginAssemblyTests.CurrentDriver.FindElement(By.Id("loginLink")).Click();
+
+            //click about, should take you to about page
+            LoginAssemblyTests.CurrentDriver.FindElement(By.Id("aboutLinkNavBar")).Click();
+
+            //check that page is the right page (Should be the about page)
+            ValidatePageTransition(LoginAssemblyTests.CurrentDriver, "Home", "About");
+
+
+            //click log in, should take you to the log in page again
+            LoginAssemblyTests.CurrentDriver.FindElement(By.Id("loginLink")).Click();
+
+            //click SetDatabase, should take you to the set database page
+            LoginAssemblyTests.CurrentDriver.FindElement(By.Id("setDatabaseNavBar")).Click();
+
+            //check that page is the right page (Should be setDatabase page)
+            ValidatePageTransition(LoginAssemblyTests.CurrentDriver, "Home", "SetDatabase");
+
+
+            //click log in, should take you to the log in page again
+            LoginAssemblyTests.CurrentDriver.FindElement(By.Id("loginLink")).Click();
+
+            //click homescreen logo in top left, should take you to homepage
+            LoginAssemblyTests.CurrentDriver.FindElement(By.Id("homeLinkNavBar")).Click();
+            
+            //check that page is the right page
+            ValidatePageTransition(LoginAssemblyTests.CurrentDriver, "Home", "Index");
+        
         }
 
         //add happy path tests for
-        // clicking any and all links - logged in or not
+        // clicking any and all links - logged in or not - from every page
         //   toggling admin privleges
         // register a user
         //  needs a test db that can be wiped for that
@@ -223,6 +202,5 @@ namespace KensUITests.Views.Accounts
         //
 
         //add tests for remeber me functionality
-        //add tests for clicking each link from the log in page
     }
 }
