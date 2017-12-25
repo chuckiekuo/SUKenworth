32d83bc22011477d75e78a63a1068f78517c4410
Troy Osland <oslandt@seattleu.edu>
2017-12-24 17:15:16 -0800

    added a file for testing persistant layout

---
+++ KensUITests/Views/Shared/Layout.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using static KensUITests.Extensions;
+
+namespace KensUITests.Views.Shared
+{
+    public class Layout
+    {
+        public static void ClickNavBarLinks(IWebDriver currentDriver, string _Controller, string _Action)
+        {
+            
+        }
+    }
+}
