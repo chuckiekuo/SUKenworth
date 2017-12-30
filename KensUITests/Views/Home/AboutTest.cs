fa66bd0e0ffab045c41ff5c6c4afd485bc40a17c
Troy Osland <oslandt@seattleu.edu>
2017-12-29 19:23:57 -0800

    added tests for the about page. PROBLEM: the ClickNavBarNoLoginLinks function in LoginPartial does not work because you have not ensured there is no one logged in. FIX IMMEDIATELY

--- KensUITests/Views/Home/AboutTest.cs
+++ KensUITests/Views/Home/AboutTest.cs
@@ -16,5 +16,52 @@ namespace KensUITests.Views.Home
     [TestClass]
     public class AboutTest
     {
+        private string _Controller = "Home";
+        private string _Action = "About";
+
+        [TestMethod]
+        public void HomeAbout_Valid_ClickAllLinks_NoLogin()
+        {
+            //navigate to the page,being tested, this function has validation built into it
+            NavigateToPage(AssemblyTests.CurrentDriver, _Controller, _Action);
+
+            //test nav bar links
+            Views.Shared.LoginPartial.ClickNavBarNoLoginLinks(AssemblyTests.CurrentDriver, _Controller, _Action);
+
+        }
+
+        [TestMethod]
+        public void HomeAbout_Valid_ClickAllLinks_AdminLogin()
+        {
+            //first log into an admin profile
+            //navigate to log in page
+            NavigateToPage(AssemblyTests.CurrentDriver, "Account", "Login");
+
+            //admin log in
+            LogIn(AssemblyTests.CurrentDriver, _Controller, _Action, ValidEmailAdmin1, ValidPasswordAdmin1);
+
+            //navigate to the page,being tested
+            NavigateToPage(AssemblyTests.CurrentDriver, _Controller, _Action);
+
+            //test nav bar links
+            Views.Shared.LoginPartial.ClickNavBarAdminLinks(AssemblyTests.CurrentDriver, _Controller, _Action, ValidEmailAdmin1, ValidPasswordAdmin1);
+        }
+
+        [TestMethod]
+        public void HomeAbout_Valid_ClickAllLinks_NonAdminLogin()
+        {
+            //first log into a nonadmin profile
+            //navigate to log in page
+            NavigateToPage(AssemblyTests.CurrentDriver, "Account", "Login");
+
+            //admin log in
+            LogIn(AssemblyTests.CurrentDriver, _Controller, _Action, ValidEmailNotAdmin1, ValidPasswordNotAdmin1);
+
+            //navigate to the page,being tested
+            NavigateToPage(AssemblyTests.CurrentDriver, _Controller, _Action);
+
+            //test nav bar links
+            Views.Shared.LoginPartial.ClickNavBarNonAdminLinks(AssemblyTests.CurrentDriver, _Controller, _Action, ValidEmailNotAdmin1, ValidPasswordNotAdmin1);
+        }
     }
 }
