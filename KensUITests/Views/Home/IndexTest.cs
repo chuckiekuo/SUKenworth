be6e2cd27ed3e52a220252d7c5a9156ce56110f8
Troy Osland <oslandt@seattleu.edu>
2017-12-29 18:20:25 -0800

    added test for homepage

--- KensUITests/Views/Home/IndexTest.cs
+++ KensUITests/Views/Home/IndexTest.cs
@@ -16,5 +16,19 @@ namespace KensUITests.Views.Home
     [TestClass]
     public class IndexTest
     {
+        private string _Controller = "Home";
+        private string _Action = "Index";
+
+        [TestMethod]
+        public void HomeIndex_Valid_ClickAllLinks_NoLogin()
+        {
+            //navigate to the page,being tested, this function has validation built into it
+            NavigateToPage(AssemblyTests.CurrentDriver, _Controller, _Action);
+
+            //test nav bar links
+            Views.Shared.LoginPartial.ClickNavBarNoLoginLinks(AssemblyTests.CurrentDriver, _Controller, _Action);
+
+        }
+
     }
 }
