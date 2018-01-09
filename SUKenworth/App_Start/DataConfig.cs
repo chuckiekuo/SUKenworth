9c6e43628c241bfc00c8f0ef36425f84fbd74cbb
William Gross <grossw@seattleu.edu>
2018-01-08 16:23:24 -0800

    Created a new and improved DataConfig
    it will be populated when each person sets up their own local test db.

---
+++ SUKenworth/App_Start/DataConfig.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SUKenworth.App_Start
+{
+    public static class DataConfig
+    {
+        public static string myLocalTestDatabase = "";
+    }
+}
